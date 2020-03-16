8a6bc4513cc32f06ba069b56fdb15dbcc1df5135
mark <xmark2@gmail.com>
2020-03-16 20:41:31 +0100

    bootstrap and Flask templates

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import os
 from setuptools import setup
 
 setup(
     name = 'my_app',
-    version = '1.0',
-    license = 'GNU General Public Licence v3',
-    author = 'xmark2',
-    author_email = 'xmark2@gmail.com',
-    description = 'Hello world app for Flask',
-    packages = ['my_app'],
-    platform = 'any',
-    install_requires = [
+    version='1.0',
+    license='GNU General Public License v3',
+    author='xmark2'
+    author_email='xmark2@gmail.com',
+    description='Hello world application for Flask',
+    packages=['my_app'],
+    platforms='any',
+    install_requires=[
         'flask',
     ],
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
