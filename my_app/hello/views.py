f7b3e35ff4864c9a907cd6feb80b818c9f10fc55
mark <xmark2@gmail.com>
2020-03-16 15:08:55 +0100

    first html template

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -3,8 +3,14 @@ from my_app.hello.models import MESSAGES
 
 hello = Blueprint('hello',__name__)
 
+from flask import render_template, request
+
 @hello.route('/')
 @hello.route('/hello')
+
+def hello_world():
+    user = request.args.get('user','Mark')
+    return render_template('index.html', user=user)
 
 def hello_world():
     return MESSAGES['default']
